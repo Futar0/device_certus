299103053c05103a0dc2d1803787a1e89705604a
Futar0 <104120854+Futar0@users.noreply.github.com>
2023-02-28 14:29:41 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_certus.mk
+    $(LOCAL_DIR)/cipher_certus.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_certus-user \
-    lineage_certus-userdebug \
-    lineage_certus-eng
+    cipher_certus-user \
+    cipher_certus-userdebug \
+    cipher_certus-eng
