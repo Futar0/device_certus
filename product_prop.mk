1712fcf1ffd78161e8b3de03c6be9dcc969db4b7
Futar0 <104120854+Futar0@users.noreply.github.com>
2023-03-06 15:54:56 +0800

    certus : Added some random commits

--- product_prop.mk
+++ product_prop.mk
@@ -75,3 +75,22 @@ PRODUCT_PRODUCT_PROPERTIES  += \
 # Iorap
   iorapd.perfetto.enable=true
   iorapd.readahead.enable=true
+
+# Dex
+dalvik.vm.dex2oat64.enabled=true 
+ro.sys.fw.dex2oat_thread_count=4 
+dalvik.vm.boot-dex2oat-threads=4
+dalvik.vm.dex2oat-cpu-set=0,1,2,3
+dalvik.vm.dex2oat-filter=quicken 
+dalvik.vm.dex2oat-threads=4
+dalvik.vm.image-dex2oat-cpu-set=0,1,2,3
+dalvik.vm.image-dex2oat-filter=quicken 
+dalvik.vm.image-dex2oat-threads=4
+
+# Dalvik Heap
+dalvik.vm.heapstartsize=8m 
+dalvik.vm.heapgrowthlimit=256m
+dalvik.vm.heapsize=512m 
+dalvik.vm.heaptargetutilization=0.75 
+dalvik.vm.heapminfree=2m 
+dalvik.vm.heapmaxfree=8m
